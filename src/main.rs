68adcaeccd4e1bf42cd4be958d644aa9b81b30cd
gioninjo <giovirighe@gmail.com>
2024-04-15 23:12:20 +0200

    height and width are now choosen by the user

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,38 @@
-use std::str::FromStr;
+use image::{imageops, open, DynamicImage, GenericImageView, Pixels, Rgba};
 use std::env;
-use image::{open, Pixels, imageops, Rgba, DynamicImage, GenericImageView};
+use std::str::FromStr;
 
-const DESIRED_HEIGHT: u32 = 64;
-const DESIRED_WIDTH: u32 = 128;
 
-fn main() -> Result<(), &'static str>{
-
+fn main() -> Result<(), &'static str> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 2 {
-        println!("Missing argument. Usage: {} <image path>", args[0]);
-        
+    if args.len() < 3 {
+        println!(
+            "Missing argument. Usage: {} <image path> <WIDTHxHEIGHT>",
+            args[0]
+        );
+
         return Err("exit now");
     }
 
-    let img_scale: Vec<char> = String::from_str(" .:-=+*#%@").unwrap_or(String::new()).chars().collect();
+    let desired_width: u32;
+    let desired_height: u32;
+
+    match parse_pair(&args[2], 'x') {
+        Some((width, height)) => {
+            desired_height = height;
+            desired_width = width;
+        },
+        None => {
+            println!("Invalid desired dimensions!!");
+            return Err("exit now");
+        }
+    }
+
+    let img_scale: Vec<char> = String::from_str(" .:-=+*#%@")
+        .unwrap_or(String::new())
+        .chars()
+        .collect();
 
     println!("{}", img_scale.len());
 
@@ -23,11 +40,16 @@ fn main() -> Result<(), &'static str>{
     let img = open(file_path).expect("Failed to open image at {file_path}");
 
     let (width, height) = img.dimensions();
-    let chunk_width = width / DESIRED_WIDTH;
-    let chunk_height = height / DESIRED_HEIGHT;
+    let chunk_width = width / desired_width;
+    let chunk_height = height / desired_height;
 
     // round up the image
-    let resized_img: DynamicImage = DynamicImage::resize(&img, chunk_width * DESIRED_WIDTH, chunk_height * DESIRED_HEIGHT, imageops::FilterType::Lanczos3);
+    let resized_img: DynamicImage = DynamicImage::resize(
+        &img,
+        chunk_width * desired_width,
+        chunk_height * desired_height,
+        imageops::FilterType::Lanczos3,
+    );
 
     let pixels: Pixels<'_, DynamicImage> = resized_img.pixels();
 
@@ -43,33 +65,46 @@ fn main() -> Result<(), &'static str>{
 
     let ascii_string: String;
 
-    match create_ascii_string(&gray_shades, &img_scale, &chunk_width, &chunk_height, &dim) {
+    match create_ascii_string(desired_width, desired_height, &gray_shades, &img_scale, &chunk_width, &chunk_height, &dim) {
         Ok(result) => ascii_string = result,
-        Err(e) => return Err(e)
+        Err(e) => return Err(e),
     }
 
-
     print!("{}", ascii_string);
-    
 
     Ok(())
-
 }
 
-
-fn create_ascii_string(gray_shades: &Vec<u32>, img_scale: &Vec<char>,chunk_width: &u32, chunk_height: &u32, (width, _height): &(u32, u32)) -> Result<String, &'static str> {
+fn create_ascii_string(
+    desired_width: u32,
+    desired_height: u32,
+    gray_shades: &Vec<u32>,
+    img_scale: &Vec<char>,
+    chunk_width: &u32,
+    chunk_height: &u32,
+    (width, _height): &(u32, u32),
+) -> Result<String, &'static str> {
     let mut ascii_string = String::new();
-    for line in 0..DESIRED_HEIGHT {
-        for column in 0..DESIRED_WIDTH {
+    for line in 0..desired_height {
+        for column in 0..desired_width {
             let start_x: u32 = column * chunk_width;
             let start_y: u32 = line * chunk_height;
             let end_x: u32 = start_x + chunk_width;
             let end_y: u32 = start_y + chunk_height;
 
-            let chunk_shade = calculate_chunk_shade(gray_shades, width, chunk_width, chunk_height, start_x, end_x, start_y, end_y);
+            let chunk_shade = calculate_chunk_shade(
+                gray_shades,
+                width,
+                chunk_width,
+                chunk_height,
+                start_x,
+                end_x,
+                start_y,
+                end_y,
+            );
             match calculate_associated_char(img_scale, chunk_shade) {
                 Ok(shade) => ascii_string.push(shade),
-                Err(e) => return Err(e)
+                Err(e) => return Err(e),
             }
         }
         ascii_string.push('\n');
@@ -78,13 +113,21 @@ fn create_ascii_string(gray_shades: &Vec<u32>, img_scale: &Vec<char>,chunk_width
     Ok(ascii_string)
 }
 
-
-fn calculate_chunk_shade(gray_shades: &Vec<u32>, width: &u32, chunk_width: &u32, chunk_height: &u32, start_x: u32, end_x: u32, start_y: u32, end_y: u32) -> u32 {
+fn calculate_chunk_shade(
+    gray_shades: &Vec<u32>,
+    width: &u32,
+    chunk_width: &u32,
+    chunk_height: &u32,
+    start_x: u32,
+    end_x: u32,
+    start_y: u32,
+    end_y: u32,
+) -> u32 {
     let mut chunk_shades_sum: u32 = 0;
 
     for pixel_y in start_y..end_y {
         for pixel_x in start_x..end_x {
-            let mut pixel_position = (pixel_y*width + pixel_x) as usize;
+            let mut pixel_position = (pixel_y * width + pixel_x) as usize;
             if pixel_position >= gray_shades.len() {
                 pixel_position = gray_shades.len() - 1;
             }
@@ -96,7 +139,10 @@ fn calculate_chunk_shade(gray_shades: &Vec<u32>, width: &u32, chunk_width: &u32,
     chunk_shades_sum / (chunk_height * chunk_width)
 }
 
-fn calculate_associated_char(img_scale: &Vec<char>, chunk_shade: u32) -> Result<char, &'static str> {
+fn calculate_associated_char(
+    img_scale: &Vec<char>,
+    chunk_shade: u32,
+) -> Result<char, &'static str> {
     if chunk_shade <= 256 {
         let mut char_index: usize = (chunk_shade * (img_scale.len() as u32) / 256) as usize;
         if char_index >= img_scale.len() {
@@ -104,11 +150,21 @@ fn calculate_associated_char(img_scale: &Vec<char>, chunk_shade: u32) -> Result<
         }
         match img_scale.get(char_index) {
             Some(res) => return Ok(res.clone()),
-            None => return Err("Selected char for shade does not exists")
+            None => return Err("Selected char for shade does not exists"),
         }
     }
 
-    println!("shade ---> {}", chunk_shade );
+    println!("shade ---> {}", chunk_shade);
 
     Err("Chunk shade number > 256!!!")
+}
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
+    }
 }
